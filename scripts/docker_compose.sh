4d67f7e8e76ce01210132b8a6cf168c65095b097
6za <53096417+6za@users.noreply.github.com>
2023-02-16 13:17:47 +0000

    feat: more details on docker compose run
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- scripts/docker_compose.sh
+++ scripts/docker_compose.sh
@@ -12,11 +12,15 @@ echo $ACTION
 # I may need the user to clone it
 # use single branch to save space
 git clone https://$GITHUB_TOKEN@$REPO_ADDRESS app
+echo "repo ok: $?"
 # cd $EXECUTION_PATH
 cd app
 docker compose ls --format json > /var/tmp/before.json
+echo "collected before state: $?"
 docker compose -f $COMPOSE_FILE $ACTION
+echo "compose run as expected:  $?"
 sleep 3
 #sleep 60
 cat /var/tmp/before.json
 docker compose ls --format json > /var/tmp/after.json
+echo "collected after state: $?"
